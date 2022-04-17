6da2f6224d9f2be36807215754e9cb4f8fec65d7
Majba <monaiburkhan@gmail.com>
2022-04-17 17:15:11 +0600

    footer add

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import './Footer.css'
 
 const Footer = () => {
     const today = new Date();
     const year = today.getFullYear();
     return (
         <footer className='text-center mt-5'>
-            <p><small>Copyright © {year} </small></p>
+       <h1 class="footer-title">Influencer products</h1>
+        <p class="footer-copyright">Copyright © 2020 influencer products</p>
+        <p class="footer-copyright">All rights reserved</p>
+            
+            
+    
         </footer>
     );
 };
